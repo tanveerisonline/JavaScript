ba9d061ebf979a374ad154787cb94b94ff1fd8a5
tanveerisonline <tanveerisonline@gmail.com>
2023-09-26 19:05:47 +0530

    Dom Manupilation

---
+++ JavaScriptBasics/dom.js
@@ -0,0 +1,38 @@
+// DOM Manipulation Document Object Model
+
+// 1. getElementById
+// const heading = document.getElementById("heading");
+// console.log(heading);
+
+// 2. getElementByTagName
+// it gets all related tags from the page
+// const headingCollection = document.getElementsByTagName("h1");
+// console.log(headingCollection[1]);
+
+// 3. getElementByClassName
+// const headingClass = document.getElementsByClassName("heading");
+// console.log(headingClass);
+
+// 4. querySelector
+// with querySelector we can target both classes and Ids.
+// const querySelId = document.querySelector("#heading");
+// console.log(querySelId)l
+// const querySelClass = document.querySelector(".heading");
+// console.log(querySelClass);
+
+// 5. querySelectorAll
+//it selects all elements that has same classes
+// const selectAll = document.querySelectorAll("heading");
+// console.log(selectAll);
+
+// 6.Traverse DOM
+// 1. parentNode
+
+// const parentNode = document.querySelector(".heading");
+// const parent = heading.parentNode;
+// console.log(parent);
+
+// 2.childNodes
+// it targets all child nodes that are inside that parent tag
+// const childNode = document.querySelector(".parent");
+// console.log(parent.childNodes);
